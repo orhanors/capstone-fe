095c32870139d75385af50b557c354e5480950c9
orhanors <orsorhan1@gmail.com>
2021-03-10 16:09:17 +0300

    inital setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "../components/NavBar/NavBar";
+
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Test from "../components/Test/Test";
+import { useTranslation } from "react-i18next";
 function App() {
+	const { i18n } = useTranslation();
 	return (
 		<div className='App'>
 			<Router>
-				<NavBar />
+				<Test />
+				<button onClick={() => i18n.changeLanguage("tr")}>
+					change
+				</button>
 			</Router>
 		</div>
 	);
